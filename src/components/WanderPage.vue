c347e594249007054839b72f8bb5824c33fea244
Renaud Fontana <renaud.fontana@enernoc.com>
2019-04-20 23:35:10 +0200

    wander page now provides links to all registered routes

--- src/components/WanderPage.vue
+++ src/components/WanderPage.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="wander">
     <h1>Not all who wander are lost</h1>
-    <code>
-    Todo:<br/>
-     - route map<br/>
-     - way home
-    </code>
+    <div class="route-map">
+      <div class="col"></div>
+      <div class="col middle">
+        <router-link-wrapper v-for="(route, index) in $router.options.routes" :key="index">
+          <router-link :to="route.path">{{ route.component.name }}</router-link>
+        </router-link-wrapper>
+      </div>
+      <div class="col"></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-@Component({})
+import RouterLinkWrapper from "./RouterLinkWrapper.vue";
+@Component({
+  components: {
+    RouterLinkWrapper
+  }
+})
 export default class WanderPage extends Vue {}
 </script>
+
+<style>
+.route-map {
+  display: flex;
+}
+.route-map > .col {
+  flex: 1 1 auto;
+}
+.route-map > .middle {
+  min-width: 320px;
+}
+</style>
